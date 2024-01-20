aeb9d178897ab896a7649dde6b4fdb5e46b0c92f
Janani Jayasuriya Withana <99182048+Janani-Withana@users.noreply.github.com>
2024-01-20 12:00:59 +0530

    Delete cinama directory

--- cinama/js/dashboard_addmovie.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    let toggle = document.querySelector(".toggle");
-    let navigation = document.querySelector(".navigation");
-    toggle.classList.toggle("active");
-    navigation.classList.toggle("active");
-}
